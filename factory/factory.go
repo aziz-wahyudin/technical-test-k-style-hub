91827147e757727c5cc32d40536c2117f41e8091
aziz-wahyudin <midnightmist37@gmail.com>
2023-04-28 06:38:03 +0700

    design program structure

---
+++ factory/factory.go
@@ -0,0 +1,16 @@
+package factory
+
+// import (
+// 	mahasiswaDelivery "aziz-wahyudin/nashta-mini-project/features/mahasiswa/delivery"
+// 	mahasiswaRepo "aziz-wahyudin/nashta-mini-project/features/mahasiswa/repository"
+// 	mahasiswaService "aziz-wahyudin/nashta-mini-project/features/mahasiswa/service"
+
+// 	"github.com/labstack/echo/v4"
+// 	"gorm.io/gorm"
+// )
+
+// func InitFactory(e *echo.Echo, db *gorm.DB) {
+// 	mahasiswaRepoFactory := mahasiswaRepo.New(db)
+// 	mahasiswaServiceFactory := mahasiswaService.New(mahasiswaRepoFactory)
+// 	mahasiswaDelivery.New(mahasiswaServiceFactory, e)
+// }
